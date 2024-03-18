2ef7aa92d7df43ffd7883b53bbc914c6e152408a
Quocbinh1 <BinhPlayll@gmail.com>
2024-03-18 22:35:37 +0700

    1h45'30s

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
     return (
-        <div>
-
+        <div className='loginsignup'>
+            <div className="loginsignup-container">
+                <h1>Sign Up</h1>
+                <div className="loginsignup-fields">
+                    <input type="text" placeholder='Your Name' />
+                    <input type="email" placeholder='Email Address' />
+                    <input type="password" placeholder='Password' />
+                </div>
+                <button>Continue</button>
+                <p className='loginsignup-login'>Bạn đã có tài khoản? <span>Đăng nhập</span></p>
+                <div className="loginsignup-agree">
+                    <input type="checkbox" name='' id='' />
+                    <p>Tôi đồng ý với điều khoản sử dụng & chính sách bảo mật.</p>
+                </div>
+            </div>
         </div>
     )
 }
